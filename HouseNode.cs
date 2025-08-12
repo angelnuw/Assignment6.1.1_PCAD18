d56d0a01b57652586f4983885abbe13a22bab27f
Angel Nuw <Angel.Nuw@mssa.newhorizons.com>
2025-08-11 22:49:36 -0400

    Add project files.

---
+++ HouseNode.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Assignment6._1._1
+{
+    public class HouseNode
+    {
+        public int HouseNumber;
+        public string Address;
+        public string HouseType;
+        public HouseNode Next;
+        public HouseNode(int number, string address, string type)
+        {
+            this.HouseNumber = number;
+            this.Address = address;
+            this.HouseType = type;
+            this.Next = null;
+        }
+    }
+
+    public class HouseLinkedList
+    {
+        private HouseNode head;
+        private HouseNode tail;
+        private int size;
+        public int Size
+        {
+            get { return size; }
+        }
+        public HouseLinkedList()
+        {
+            head = null;
+            tail = null;
+            size = 0;
+        }
+
+        public bool IsEmpty()
+        {
+            return head == null;
+        }
+
+        public void AddFirst(int number, string address, string type)
+        {
+            if (IsHouseNumberExists(number))
+            {
+                Console.WriteLine("House number already exists.");
+                return;
+            }
+
+            var newNode = new HouseNode(number, address, type);
+
+            if (IsEmpty())
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                newNode.Next = head;
+                head = newNode;
+            }
+            size++;
+        }
+        public void Display()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("List is empty");
+                return;
+            }
+
+            var cur = head;
+            Console.WriteLine("Number | Address | Type");
+            while (cur != null)
+            {
+                Console.WriteLine($"#{cur.HouseNumber} | {cur.Address} | {cur.HouseType}");
+                cur = cur.Next;
+            }
+        }
+        private bool IsHouseNumberExists(int number)
+        {
+            var current = head;
+            while (current != null)
+            {
+                if (current.HouseNumber == number)
+                    return true;
+                current = current.Next;
+            }
+            return false;
+        }
+
+        public void AddLast(int number, string address, string type)
+        {
+            if (IsHouseNumberExists(number))
+            {
+                Console.WriteLine("House number already exists.");
+                return;
+            }
+
+            var newNode = new HouseNode(number, address, type);
+
+            if (IsEmpty())
+            {
+                head = tail = newNode;
+            }
+            else
+            {
+                tail.Next = newNode;
+                tail = newNode;
+            }
+            size++;
+        }
+        public HouseNode SearchByNumber(int number)
+        {
+            var cur = head;
+            while (cur != null)
+            {
+                if (cur.HouseNumber == number)
+                {
+                    return cur;
+                }
+                cur = cur.Next;
+            }
+            return null;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Assignment6._1._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("\nLinked List of Houses: ");
+            HouseLinkedList mylist = new HouseLinkedList();
+            mylist.AddLast(1, "123 Main St", "Single Family");
+            mylist.AddLast(2, "456 Elm St", "Condo");
+            mylist.AddLast(3, "789 Oak St", "Townhouse");
+            mylist.AddLast(4, "101 Pine St", "Single Family");
+            mylist.Display();
+
+            Console.Write("\nEnter a house number to search: ");
+            if (int.TryParse(Console.ReadLine(), out int searchNum))
+            {
+                var found = mylist.SearchByNumber(searchNum);
+                if (found != null)
+                {
+                    Console.WriteLine($"House found: #{found.HouseNumber} | {found.Address} | {found.HouseType}");
+                }
+                else
+                {
+                    Console.WriteLine("House not found.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a valid house number.");
+            }
+        }
+    }
+}
